64d7d18a6a94f053c819b5e240c98d194a6b876d
Chang Su <CatherineSue@users.noreply.github.com>
2018-11-20 19:59:25 -0800

    Add sample text for artists (#7)

--- crwal.py
+++ crwal.py
@@ -19,7 +19,7 @@ def fetch_songs_from_api(artist):
     urlretrieve(api_url, saved_file)
 
     records = []
-    with open(saved_file, encoding="utf-8") as f:
+    with open(saved_file, "r", encoding="utf-8") as f:
         results = json.load(f)
         for result in results["results"]:
             record = dict()
@@ -30,6 +30,12 @@ def fetch_songs_from_api(artist):
             records.append(record)
 
     return records
+
+
+def load_artists(filename):
+    with open(filename, "r", encoding="utf-8") as f:
+        artists = [line.strip() for line in f]
+    return artists
 
 
 def get_songs_of_artists(artists):
@@ -46,7 +52,8 @@ def get_songs_of_artists(artists):
 # 2. Multiple genre labels.
 
 if __name__ == '__main__':
-    artists = ["Michael Jackson", "Kris Wu"]
+    artists = load_artists("./data/artists.txt")
+    # print(artists)
     records = get_songs_of_artists(artists)
-    for r in records:
-        print(r)
+    # for r in records:
+    #     print(r)
