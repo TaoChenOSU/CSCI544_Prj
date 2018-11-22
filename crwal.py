28bebfbdfd0ebb16a19490848fd50d82a7ef9676
Chang Su <CatherineSue@users.noreply.github.com>
2018-11-21 18:22:17 -0800

    Add song_records.txt and .gitignore (#8)

--- crwal.py
+++ crwal.py
@@ -1,35 +1,7 @@
+import glob
 import json
+import time
 from urllib.request import urlretrieve
-
-
-def fetch_songs_from_api(artist):
-    """
-    Fetch a list of song records of an artist from web. 
-
-    Arg:
-        artist, the name of the artist, e.g. "Ariana Grande"
-    Return:
-        a list of records: [{"artist":, "song_name":, "genre":, "alternate_artist"}]
-    """
-
-    api_url = "https://itunes.apple.com/search?term={}+{}&media=music&entity=song&limit=200".format(
-        *artist.split(" "))
-    # print(api_url)
-    saved_file = "./data/{}.txt".format(artist)
-    urlretrieve(api_url, saved_file)
-
-    records = []
-    with open(saved_file, "r", encoding="utf-8") as f:
-        results = json.load(f)
-        for result in results["results"]:
-            record = dict()
-            record["artist"] = result["artistName"]
-            record["song_name"] = result["trackName"]
-            record["genre"] = result["primaryGenreName"]
-            record["alternate_artist"] = artist
-            records.append(record)
-
-    return records
 
 
 def load_artists(filename):
@@ -38,13 +10,56 @@ def load_artists(filename):
     return artists
 
 
-def get_songs_of_artists(artists):
-    records = []
-    for artist in artists:
-        song_genres_i = fetch_songs_from_api(artist)
-        records.extend(song_genres_i)
+def fetch_songs_from_api(artists, output_dir):
+    """
+    Fetch a list of song records of artists from web and save to output_dir.
 
-    return records
+    Arg:
+        artists: a list of artist names, e.g. "[Ariana Grande,]"
+    """
+    na = len(artists)
+    i = 83
+
+    while i < na:
+        artist = artists[i]
+        try:
+            api_url = "https://itunes.apple.com/search?term={}&media=music&entity=song&limit=200".format(
+                ("+").join(artist.split(" ")))
+            response_data = output_dir + "{}.txt".format(artist)
+            urlretrieve(api_url, response_data)
+
+            print("Completed index {}, artist {}".format(i, artist))
+            time.sleep(40)
+            i += 1
+        except Exception as e:
+            print("Encounter error {} at index {}, artist {}".format(
+                e, i, artist))
+            time.sleep(200)
+
+
+def extract_records(input_dir, output):
+    """
+    Extracts song records in the input_dir and output them to output.
+
+    Arg:
+        input_dir:
+        output:
+    """
+    fo = open(output, "w", encoding="utf-8")
+
+    for file in glob.glob(input_dir + "*"):
+        with open(file, "r", encoding="utf-8") as fin:
+            results = json.load(fin)
+            artist_search_term = file.split("/")[-1].split(".")[0]
+            for result in results["results"]:
+                record = dict()
+                record["artist"] = result["artistName"]
+                record["song"] = result["trackName"]
+                record["genre"] = result["primaryGenreName"]
+                record["alternative_artist"] = artist_search_term
+                fo.write("{}\n".format(record))
+
+    fo.close()
 
 
 # TODO:
@@ -52,8 +67,10 @@ def get_songs_of_artists(artists):
 # 2. Multiple genre labels.
 
 if __name__ == '__main__':
-    artists = load_artists("./data/artists.txt")
-    # print(artists)
-    records = get_songs_of_artists(artists)
-    # for r in records:
-    #     print(r)
+    input_file = "./data/artnames_all_genres.txt"
+    output_file = "./data/song_records.txt"
+    records_dir = "./data/api/"
+
+    # artists = load_artists(input_file)
+    # records = fetch_songs_from_api(artists, records_dir)
+    extract_records(records_dir, output_file)
