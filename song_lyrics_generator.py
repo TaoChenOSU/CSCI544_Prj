d93bf2171c94e98c5544b22bb0ff01fe7d25907c
Tao Chen <taochen@usc.edu>
2018-11-22 16:11:53 -0800

    Test lyrics generator
    Created lyrics subfolder.

--- song_lyrics_generator.py
+++ song_lyrics_generator.py
@@ -6,6 +6,7 @@ import requests
 
 
 description_text = "description~~~"
+headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36'}
 
 def read_records_json(records_dir):
       genre_records_map = dict()
@@ -23,15 +24,12 @@ def get_lyrics_of_songs(genre_records_map):
 
     path = "./data/lyrics/"
 
-    headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36'}
-    artists_songs = dict()
-
     json_dict = dict()
     json_dict["description"] = description_text
     json_dict["corpus"] = []
     for key, records in genre_records_map.items():
         with open(path + "test_{}.json".format(key), "w") as output:
-            for record in records[:600]:    # for each label, only fetches the first 600 songs, to save time
+            for record in records[:10]:    # for each label, only fetches the first 600 songs, to save time
                 artist = record["artist"]
                 song_name = record["song"]
 
@@ -74,16 +72,9 @@ def get_lyrics_of_songs(genre_records_map):
             json.dump(json_dict, output, indent=4)
 
 
-# TODO:
-# 1. Data cleansing: i.e. repeat data # deleted
-# 2. Multiple genre labels. # deleted
-
 if __name__ == '__main__':
     records_dir = "./data/genre/"
 
-    # artists = load_artists(input_file)
-    # fetch_songs_from_api(artists, records_dir)
-    # genres = extract_records(records_dir, output_file, summary_file)
     records = read_records_json(records_dir)
     # print(records)
     get_lyrics_of_songs(records)
